f6100eaf07d7e8a2a50dfe09c66722a8d4d76a95
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2023-06-19 09:25:59 +0600

    Bannder added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
     }
     return (
-        <nav className=" py-6">
+        <nav className="max-w-7xl mx-auto py-6">
             <div className=" px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
@@ -70,8 +70,8 @@ const NavBar = () => {
                         <div className="ml-4 flex items-center md:ml-6">
                             <a href="#" className="text-gray-600 hover:text-green-500 duration-500 px-3 py-2 rounded-md text-lg font-medium">Login </a>
                             <div className="ml-3">
-                                <a href="#" className="text-lg rounded-full border-2  hover:bg-gradient-to-l
-                                 to-blue-500 to-95% from-indigo-500 duration-500  hover:text-white py-2 px-5 uppercase" >Buy Course </a>
+                                <a href="#" className="text-lg rounded-full border border-green-500  hover:bg-gradient-to-l
+                                 to-[#0f8]  from-[#fe5e5e] duration-500  hover:text-white py-2 px-5 uppercase" >Buy Course </a>
                             </div>
                         </div>
                     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
+  <div>
     <RouterProvider router={router} />
   </div>,
 )
